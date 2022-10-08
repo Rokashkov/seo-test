c013f36ee2c8c69110e2802e3404a2e2244ebc7b
Vladimir <vovalol31@gmail.com>
2022-10-08 12:16:21 +0300

    robots.txt added

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ app.listen(PORT, () => {
 	console.log(`Server has been started on PORT: ${ PORT }...`)
 })
 
+app.use(express.static('public'))
+
 app.get('*', (req, res) => {
 	res.send('Hello, World!')
 })
